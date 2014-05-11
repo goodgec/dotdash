49c08384a797e2846c9b45108de4e5eec59cb07c
rachelAdams <adamsr@carleton.edu>
2014-05-11 12:07:31 -0500

    Added widget icons, send button on new msg screen
    Still needs morse tap button to change color when pressed.

--- DotDash/DotDash/src/main/java/com/groupa/dotdash/dotdash/NewMessageActivity.java
+++ DotDash/DotDash/src/main/java/com/groupa/dotdash/dotdash/NewMessageActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class NewMessageActivity extends DotDash {
@@ -13,9 +15,10 @@ public class NewMessageActivity extends DotDash {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
         currentScreen = R.id.action_compose;
+        final Button morseButton = (Button) findViewById(R.id.morseTapButton);
+
+
 
     }
-
-
 
 }
